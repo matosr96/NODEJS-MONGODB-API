55c339889bcb8ec094c723f381ad55f5504e714c
matosr96 <19matos96@gmail.com>
2022-08-01 23:24:19 -0500

    finish crud

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,49 @@
 import User from "../models/users.model.js";
 
-export const crearUsuario = async (req, res) => {
-  const user = User(req.body);
-  user
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+export const createUser = async (req, res) => {
+  try {
+    const user = User(req.body);
+    user.save().then((data) => res.json(data));
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+export const getUsers = (req, res) => {
+  try {
+    User.find().then((data) => res.json(data));
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+export const getUser = (req, res) => {
+  const { id } = req.params;
+  try {
+    User.findById(id).then((data) => res.json(data));
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+export const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { name, age, email, password } = req.body;
+  try {
+    User.updateOne({ _id: id }, { $set: { name, age, email, password } }).then(
+      (data) => res.json(data)
+    );
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
+
+  try {
+    User.remove({ _id: id }).then((data) => res.json(data));
+  } catch (error) {
+    res.json({ message: error });
+  }
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { crearUsuario } from "../controllers/user.controller.js";
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.post("/user", crearUsuario);
+router.post("/user", createUser);
+router.get("/user", getUsers);
+router.get("/user/:id", getUser);
+router.put("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
 
 export default router;
